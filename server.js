af187dec18099a454eb4de85a318082312d0b689
Lonnie Root <root.lonnie@gmail.com>
2023-02-14 15:08:50 -0500

    beginning phases

---
+++ server.js
@@ -0,0 +1,6 @@
+// Dependencies & Express Data Parsing
+const fs = require('fs');
+const express = require("express");
+const path = require("path");
+const app = express();
+const PORT = process.env.PORT || 3001;
